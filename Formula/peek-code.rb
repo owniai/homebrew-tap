999352ea5487ad1f25a97456602ba5e38461f66a
owniai <noreply@owniai.com>
2026-05-28 03:20:26 +0000

    bump peek to v0.3.0

--- Formula/peek-code.rb
+++ Formula/peek-code.rb
@@ -1,28 +1,28 @@
 class PeekCode < Formula
   desc "Multi-language code definition search powered by tree-sitter"
   homepage "https://github.com/owniai/peek"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/owniai/peek/releases/download/v0.2.1/peek-v0.2.1-aarch64-apple-darwin.tar.gz"
-      sha256 "e91a993a4adf5d9e2a7484f485dd11efce389aa4a7d7554bd60e16181ab41fe5"
+      url "https://github.com/owniai/peek/releases/download/v0.3.0/peek-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "48b17648a8d0fb68c713b31042bff0d7a01fc49c9005128432dc97fe75aec521"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/owniai/peek/releases/download/v0.2.1/peek-v0.2.1-x86_64-apple-darwin.tar.gz"
-      sha256 "f59893f020a334fb4a9b9b6fcd2264c632373f7394d89b34c5d4fc7a57b209bb"
+      url "https://github.com/owniai/peek/releases/download/v0.3.0/peek-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "488fc7d85f54105a35e65cc3518b9ae120e7a5f90136ffcc9befbec30c9367b8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/owniai/peek/releases/download/v0.2.1/peek-v0.2.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "cdd1bf01a0489d411f8fd1b96437d94b977cd57969daeb5555e0ff8b7e430862"
+      url "https://github.com/owniai/peek/releases/download/v0.3.0/peek-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "b716f3182567166d1ff0ff159de7cd2a0abd22430e24c35a2d37179da4e84d4c"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/owniai/peek/releases/download/v0.2.1/peek-v0.2.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e9facbce8db8a069431fb85465a94645280689cc78ae1697552a9e60563ac170"
+      url "https://github.com/owniai/peek/releases/download/v0.3.0/peek-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "5d04ab2c85802f05d1a7cc772670708ab6a9477919599bc38b1927e6ce1de66d"
     end
   end
 
